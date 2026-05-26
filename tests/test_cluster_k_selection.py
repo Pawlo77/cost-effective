e616130f7663d6ac48245d937a59cf6deb1ee49b
Pawel <pawflor@gmail.com>
2026-05-26 12:10:32 +0200

    fix: tests

--- tests/test_cluster_k_selection.py
+++ tests/test_cluster_k_selection.py
@@ -3,16 +3,7 @@
 import numpy as np
 import pandas as pd
 
-from cost_effective.models.cluster_k_selection import (
-    kmeans_k_diagnostics,
-    suggest_elbow_k,
-)
-
-
-def test_elbow_k_on_simple_curve() -> None:
-    k = np.array([2, 3, 4, 5])
-    inertia = np.array([100.0, 40.0, 35.0, 33.0])
-    assert suggest_elbow_k(k, inertia) == 3
+from cost_effective.models.cluster_k_selection import kmeans_k_diagnostics
 
 
 def test_kmeans_k_diagnostics_columns() -> None:
